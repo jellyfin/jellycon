98e7948a4c939ddf9465eb73a22be5b8d7544a1b
sfaulds <sfaulds@connexity.com>
2017-04-10 09:19:08 +1000

    shorten name again to EmbyCon code refactor for service to reduce static code inspection warning

--- service.py
+++ service.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # Gnu General Public License - see LICENSE.TXT
 
 import xbmc
@@ -10,124 +11,126 @@ from resources.lib.downloadutils import DownloadUtils
 from resources.lib.simple_logging import SimpleLogging
 
 log = SimpleLogging("EmbyCon.service")
-downloadUtils = DownloadUtils()
+download_utils = DownloadUtils()
 
 # auth the service
 try:
-    downloadUtils.authenticate()
+    download_utils.authenticate()
 except Exception, e:
     pass
 
-newWebSocketThread = WebSocketThread()
-newWebSocketThread.setDaemon(True)
-newWebSocketThread.start()
-    
+websocket_thread = WebSocketThread()
+websocket_thread.setDaemon(True)
+websocket_thread.start()
+
+
 def hasData(data):
-    if(data == None or len(data) == 0 or data == "None"):
+    if data is None or len(data) == 0 or data == "None":
         return False
     else:
         return True
-        
+
+
 def stopAll(played_information):
 
-    if(len(played_information) == 0):
-        return 
+    if len(played_information) == 0:
+        return
         
     log.info("played_information : " + str(played_information))
     
     for item_url in played_information:
         data = played_information.get(item_url)
-        if(data != None):
+        if data is not None:
             log.info("item_url  : " + item_url)
             log.info("item_data : " + str(data))
             
-            currentPossition = data.get("currentPossition")
-            item_id = data.get("item_id")
+            current_possition = data.get("currentPossition")
+            emby_item_id = data.get("item_id")
             
-            if(hasData(item_id)):
-                log.info("Playback Stopped at: " + str(int(currentPossition * 10000000)))
-                newWebSocketThread.playbackStopped(item_id, str(int(currentPossition * 10000000)))
+            if hasData(emby_item_id):
+                log.info("Playback Stopped at: " + str(int(current_possition * 10000000)))
+                websocket_thread.playbackStopped(emby_item_id, str(int(current_possition * 10000000)))
         
     played_information.clear()
     
     
-class Service( xbmc.Player ):
+class Service(xbmc.Player):
 
     played_information = {}
     
-    def __init__( self, *args ):
-        log.info("EmbyCon Service -> starting monitor service")
+    def __init__(self, *args):
+        log.info("Starting monitor service: " + str(args))
         self.played_information = {}
         pass
     
-    def onPlayBackStarted( self ):
+    def onPlayBackStarted(self):
         # Will be called when xbmc starts playing a file
         stopAll(self.played_information)
         
-        currentFile = xbmc.Player().getPlayingFile()
-        log.info("onPlayBackStarted" + currentFile)
+        current_playing_file = xbmc.Player().getPlayingFile()
+        log.info("onPlayBackStarted" + current_playing_file)
         
-        WINDOW = xbmcgui.Window( 10000 )
-        item_id = WINDOW.getProperty("playback_url_" + currentFile)
-        log.info("item_id: " + item_id)
+        window_handle = xbmcgui.Window(10000)
+        emby_item_id = window_handle.getProperty("playback_url_" + current_playing_file)
+        log.info("item_id: " + emby_item_id)
 
-        # reset all these so they dont get used is xbmc plays a none 
-        #WINDOW.setProperty("item_id", "")
-        
-        if(item_id == None or len(item_id) == 0):
+        if emby_item_id is None or len(emby_item_id) == 0:
             return
         
-        newWebSocketThread.playbackStarted(item_id)
+        websocket_thread.playbackStarted(emby_item_id)
         
         data = {}
-        data["item_id"] = item_id
-        self.played_information[currentFile] = data
+        data["item_id"] = emby_item_id
+        self.played_information[current_playing_file] = data
         
-        log.info("EmbyCon Service -> ADDING_FILE : " + currentFile)
-        log.info("EmbyCon Service -> ADDING_FILE : " + str(self.played_information))
+        log.info("ADDING_FILE : " + current_playing_file)
+        log.info("ADDING_FILE : " + str(self.played_information))
 
-    def onPlayBackEnded( self ):
+    def onPlayBackEnded(self):
         # Will be called when xbmc stops playing a file
         log.info("EmbyCon Service -> onPlayBackEnded")
         stopAll(self.played_information)
 
-    def onPlayBackStopped( self ):
+    def onPlayBackStopped(self):
         # Will be called when user stops xbmc playing a file
-        log.info("EmbyCon Service -> onPlayBackStopped")
+        log.info("onPlayBackStopped")
         stopAll(self.played_information)
 
 monitor = Service()
-lastProgressUpdate = datetime.today()
+last_progress_update = datetime.today()
             
 while not xbmc.abortRequested:
     
     if xbmc.Player().isPlaying():
         try:
             # send update
-            td = datetime.today() - lastProgressUpdate
-            secDiff = td.seconds
-            if(secDiff > 5):
+            td = datetime.today() - last_progress_update
+            sec_diff = td.seconds
+            if sec_diff > 5:
             
-                playTime = xbmc.Player().getTime()
-                currentFile = xbmc.Player().getPlayingFile()
+                play_time = xbmc.Player().getTime()
+                current_file = xbmc.Player().getPlayingFile()
                 
-                if(monitor.played_information.get(currentFile) != None):
-                    monitor.played_information[currentFile]["currentPossition"] = playTime            
+                if monitor.played_information.get(current_file) is not None:
+
+                    monitor.played_information[current_file]["currentPossition"] = play_time
             
-                if(monitor.played_information.get(currentFile) != None and monitor.played_information.get(currentFile).get("item_id") != None):
-                    item_id =  monitor.played_information.get(currentFile).get("item_id")
-                    newWebSocketThread.sendProgressUpdate(item_id, str(int(playTime * 10000000)))
+                if (monitor.played_information.get(current_file) is not None and
+                        monitor.played_information.get(current_file).get("item_id") is not None):
+
+                    item_id = monitor.played_information.get(current_file).get("item_id")
+                    websocket_thread.sendProgressUpdate(item_id, str(int(play_time * 10000000)))
                     
-                lastProgressUpdate = datetime.today()
+                last_progress_update = datetime.today()
             
         except Exception, e:
-            log.error("EmbyCon Service -> Exception in Playback Monitor : " + str(e))
+            log.error("Exception in Playback Monitor : " + str(e))
             pass
 
     xbmc.sleep(1000)
     xbmcgui.Window(10000).setProperty("EmbyCon_Service_Timestamp", str(int(time.time())))
     
 # stop the WebSocket client
-newWebSocketThread.stopClient()
+websocket_thread.stopClient()
 
-log.info("EmbyCon Service -> Service shutting down")
+log.info("Service shutting down")
