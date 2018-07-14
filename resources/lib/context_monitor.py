4d4ad7812a491481b95e3e71a32cf97ca3642413
faush01 <shaun@bluebit.com.au>
2018-07-15 09:29:09 +1000

    use the container(id).ListItem call to monitor for EmbyCon addon items and show the EmbyCon context dialog

--- resources/lib/context_monitor.py
+++ resources/lib/context_monitor.py
@@ -11,31 +11,38 @@ log = SimpleLogging(__name__)
 
 class ContextMonitor(threading.Thread):
 
-    stop_monitor = False
+    stop_thread = False
 
     def run(self):
 
-        #condition_command = "(Window.IsVisible(DialogContextMenu.xml) + !String.IsEmpty(ListItem.Property(id)) + String.StartsWith(ListItem.Path,plugin://plugin.video.embycon))"
-        condition_command = ("Window.IsVisible(DialogContextMenu.xml) + " +
-                             "[Window.IsActive(Home) | " +
-                             "[!String.IsEmpty(ListItem.Property(id) + " +
-                             "String.StartsWith(ListItem.Path,plugin://plugin.video.embycon)]]")
-
-        monitor = xbmc.Monitor()
+        item_id = None
         log.debug("ContextMonitor Thread Started")
+        context_up = False
+        is_embycon_item = False
 
-        while not xbmc.abortRequested:
+        while not xbmc.abortRequested and not self.stop_thread:
 
-            if xbmc.getCondVisibility(condition_command):
-                log.debug("ContextMonitor Found Context Menu!!!!!!")
-                xbmc.executebuiltin("Dialog.Close(contextmenu, true)")
-
-                item_id = xbmc.getInfoLabel('ListItem.Property(id)')
-                if item_id:
+            if xbmc.getCondVisibility("Window.IsVisible(contextmenu)"):
+                context_up = True
+                if is_embycon_item:
+                    xbmc.executebuiltin("Dialog.Close(contextmenu,true)")
+            else:
+                if context_up:  # context now down, do something
+                    context_up = False
+                    container_id = xbmc.getInfoLabel("System.CurrentControlID")
+                    log.debug("ContextMonitor Container ID: {0}", container_id)
+                    item_id = xbmc.getInfoLabel("Container(" + str(container_id) + ").ListItem.Property(id)")
                     log.debug("ContextMonitor Item ID: {0}", item_id)
-                    params = {}
-                    params["item_id"] = item_id
-                    show_menu(params)
+                    if item_id:
+                        params = {}
+                        params["item_id"] = item_id
+                        show_menu(params)
+
+            container_id = xbmc.getInfoLabel("System.CurrentControlID")
+            condition = ("String.StartsWith(Container(" + str(container_id) +
+                         ").ListItem.Path,plugin://plugin.video.embycon) + !String.IsEmpty(Container(" +
+                         str(container_id) + ").ListItem.Property(id))")
+            is_embycon_item = xbmc.getCondVisibility(condition)
 
             xbmc.sleep(200)
 
@@ -43,4 +50,4 @@ class ContextMonitor(threading.Thread):
 
     def stop_monitor(self):
         log.debug("ContextMonitor Stop Called")
-        self.stop_monitor = True
+        self.stop_thread = True
