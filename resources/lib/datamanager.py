ac0133ff788896539d767cc195630a6b9e524b3f
Shaun <shaun@bluebit.com.au>
2018-12-25 07:22:37 +1100

    log is caching or not

--- resources/lib/datamanager.py
+++ resources/lib/datamanager.py
@@ -52,7 +52,7 @@ class DataManager():
     def get_items(self, url, gui_options, use_cache=False):
 
         home_window = HomeWindow()
-        log.debug("last_content_url : {0}", url)
+        log.debug("last_content_url : use_cache={0} url={1}", use_cache, url)
         home_window.setProperty("last_content_url", url)
 
         user_id = DownloadUtils().getUserId()
