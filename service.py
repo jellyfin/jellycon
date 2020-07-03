6015bb9e58145c7f7e8194a9a3e2ae030115e927
faush01 <shaun@bluebit.com.au>
2020-07-03 16:53:04 +1000

    reload settings before suing them in the service

--- service.py
+++ service.py
@@ -139,6 +139,7 @@ while not xbmc.abortRequested:
                     user_changed = True
 
                 if user_changed or first_run:
+                    settings = xbmcaddon.Addon()
                     server_speed_check_data = settings.getSetting("server_speed_check_data")
                     server_host = download_utils.get_server()
                     if server_host is not None and server_host != "" and server_host != "<none>" and server_host not in server_speed_check_data:
